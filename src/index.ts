1c585273e6aee19d792cd7030e9d161325eb0035
Humberto Piaia <betopiaia@gmail.com>
2022-07-08 14:29:58 -0300

    Finish first implementaton of forwarder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,91 @@
-export default function createLoader() {}
+import { json } from '@remix-run/node'
+
+/**
+ * Creates a new instance of forwarder.
+ *
+ * @param config - The configuration object.
+ * @param config.baseUrl - The base url of the API.
+ * @param config.errorHandler - The function to call when a forwarder error occurs.
+ * @returns A new instance of forwarder.
+ */
+export default function createForwarder({
+  baseUrl,
+  errorHandler,
+}: {
+  baseUrl: string
+  errorHandler: (error: Response) => Response
+}) {
+  return {
+    /**
+     * Forward an action to the given endpoint.
+     *
+     * @param options - The options to forward.
+     * @param options.request - The request to forward.
+     * @param options.to - The endpoint to forward to.
+     * @param options.transform - A function to transform the response.
+     * @returns Promise<Response> - The response from the forward.
+     */
+    forwardAction: async <Data>({
+      request,
+      to,
+      transform,
+    }: {
+      request: Request
+      to: string
+      transform?: (result: Data) => Response
+    }) => {
+      const body = await request.formData()
+
+      const response = await fetch(baseUrl + to, {
+        method: request.method,
+        body,
+      })
+
+      if (!response.ok) {
+        return errorHandler ? errorHandler(response) : response
+      }
+
+      return transform ? transform(await response.json()) : response
+    },
+
+    /**
+     * Forward a loader to the given set of endpoints.
+     *
+     * @param options - The options to forward.
+     * @param options.request - The request to forward.
+     * @param options.to - A set of endpoints to forward to.
+     * @param options.transform - A function to transform the response.
+     * @returns Promise<Response> - The response from the forward.
+     */
+    forwardLoader: async <Data = any>({
+      request,
+      to,
+      transform,
+    }: {
+      request: Request
+      to: Record<keyof Data, string>
+      transform?: (result: Data) => Response
+    }) => {
+      const data: any = {}
+
+      for (const [key, url] of Object.entries<string>(to)) {
+        const urlWithParameters = url.replace(
+          /:([a-zA-Z0-9_]+)/g,
+          (_, name: string) => new URL(request.url).searchParams.get(name) || ''
+        )
+
+        const response = await fetch(baseUrl + urlWithParameters, {
+          method: request.method,
+        })
+
+        if (!response.ok) {
+          return errorHandler ? errorHandler(response) : response
+        }
+
+        data[key] = await response.json()
+      }
+
+      return transform ? transform(data) : json(data)
+    },
+  }
+}
